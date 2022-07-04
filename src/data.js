0a3e3c34250e562dc84aebb50d6993729e0de008
Ryan Lackey <lackeyr13@gmail.com>
2022-07-03 21:51:56 -0400

    initial commit

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    title: "Sick Tunez",
+    subtitle: "1st group project",
+    description:
+    "This apps function is to allow the user to look up artists or bands, select a album or song and view the infomation and lyrics as well",
+    image: '/images/Sicktune.jpeg',
+    link: "https://reactbootcamp.com", 
+  },
+  {
+    title: "React Project",
+    subtitle: "Group React Project",
+    description:
+      "This project was created by three other and myself, the funtion was to book a daycare stay at a doggy hotel",
+    image: "/images/Sicktune.jpeg",
+    link: "https://reactbootcamp.com",
+  }, 
+
+  {
+    title: "Weather App", 
+    subtitle: "Weather Dashboard",
+    description: 
+    "This app allows users to check the weather of any city of interest. Using a Weather API React Bootstrap",
+    image: "./images/project-3.gif",
+    link: "https://ryryit.github.io/WeatherDashboard/",
+  },
+  {
+    title: "Employee Directory",
+    subtitle: "React Employee look up",
+    description:
+      "React based app used to look up employee information. App was created during my Full-stack BootcampS",
+    images: "sicktune.jpeg",
+    link: "https://github.com/RyRyIT/User-Directory.git",
+  }, 
+]; 
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    projectimage: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "",
+    image: "",
+    name: "",
+    company: "",
+  },
+]; 
+
+export const skills = [
+  "JavaScript", 
+  "PHP", 
+  "GIT",
+  "React",
+  "Node",
+  "MYSQL", 
+  "Wordpress"
+
+];
